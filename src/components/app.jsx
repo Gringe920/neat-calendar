a1bf174bc6cd53092e53b0b6f4be4c2a4d9eca5b
Gringe920 <e-dam@163.com>
2018-01-29 16:36:49 +0800

    Add: add some test file

---
+++ src/components/app.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default class App extends React.Component{
+    contructor(props){
+
+    }
+
+    render(){
+        return(
+            <div className="test-container">
+                okok
+            </div>
+        )
+    }
+}
