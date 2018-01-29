ea977809e06023f4ed90105662c290f2af5bb5b4
Gringe920 <e-dam@163.com>
2018-01-29 16:36:25 +0800

    Add: add webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,33 @@
+
+const path = require('path');
+
 module.export = {
-    entry: '',
+    entry: path.join(__dirname, 'src', 'index.jsx'),
     output: {
-        
+        path: path.resolve(__dirname, 'src', 'bundle'),
+        filename: '[name].bundle.js',
+        publicPath: 'bundle'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.scss$/,
+                exclude: /node_modules/,
+                loader: ['style-loader', 'css-loader', 'sass-loader']
+            },
+            {
+                test:/\.js|jsx$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            }
+        ]
+    },
+    resolve:{
+        extensions: ['.js', '.jsx', '.scss']
+    },
+    devServer: {
+        historyApiFallback: {
+            index: 'view/index.html'
+        }
     }
-}
+};
